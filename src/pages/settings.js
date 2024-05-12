9b7267caec516f1b908e9cb8d262d48b1781342e
Japerz <30984308+Japerz12138@users.noreply.github.com>
2024-05-12 13:29:55 -0400

    Update settings.js
    - Delete "save" button since it now auto saves
    - After user modified the settings, it will now show a toast indicate the setting has been saved
    - Added "App Info" section display app version and developer info etc.

--- src/pages/settings.js
+++ src/pages/settings.js
@@ -1,13 +1,15 @@
 import React, { useEffect, useRef, useState } from 'react';
-import { Dropdown } from 'react-bootstrap';
+import { Dropdown, Toast } from 'react-bootstrap';
 const { version } = require('../../package.json');
 
 function Settings() {
 
+    const [showSavedToast, setShowSavedToast] = useState(false);
     const [selectedDirectoryPath, setSelectedDirectoryPath] = useState('');
     const [selectedTheme, setSelectedTheme] = useState('Dark');
     const [selectedThreadNumber, setSelectedThreadNumber] = useState('');
     const fileInputRef = useRef(null);
+    const [developerInfo, setDeveloperInfo] = useState('');
 
     useEffect(() => {
         window.electron.getSettings().then((settings) => {
@@ -15,6 +17,10 @@ function Settings() {
             setSelectedTheme(settings.theme);
             setSelectedThreadNumber(settings.threadNumber);
         });
+    }, []);
+
+    useEffect(() => {
+        setDeveloperInfo('Developed by Team RDM');
     }, []);
 
     const handleDirectoryChange = (event) => {
@@ -25,6 +31,8 @@ function Settings() {
             setSelectedDirectoryPath(path);
             console.log(selectedDirectory.path);
             window.electron.saveSettings({ directoryPath: path });
+            setShowSavedToast(true);
+            setTimeout(() => setShowSavedToast(false), 2000);
         }
     };
 
@@ -36,23 +44,30 @@ function Settings() {
     const handleThemeChange = (theme) => {
         setSelectedTheme(theme);
         window.electron.saveSettings({ theme: theme });
+        setShowSavedToast(true);
+        setTimeout(() => setShowSavedToast(false), 2000);
 
         if (theme === 'Dark') {
             const htmlElement = document.querySelector('html');
             htmlElement.setAttribute('data-bs-theme', 'dark');
-          } else {
+        } else {
             const htmlElement = document.querySelector('html');
             htmlElement.setAttribute('data-bs-theme', 'light');
-          }
+        }
     };
 
     const handleThreadNumberChange = (number) => {
         setSelectedThreadNumber(number);
         window.electron.saveSettings({ threadNumber: number });
+        setShowSavedToast(true);
+        setTimeout(() => setShowSavedToast(false), 2000);
     };
 
     return (
         <div className="Settings">
+            <Toast show={showSavedToast} onClose={() => setShowSavedToast(false)} style={{ position: 'fixed', top: '0', right: '0', margin: '1rem', zIndex: '10000' }}>
+                <Toast.Body className="text-center"><i class="bi bi-floppy"></i>  Setting has been saved</Toast.Body>
+            </Toast>
             <div className="container" style={{ marginTop: '100px' }}>
                 <div className="row">
                     <div className="col">
@@ -150,10 +165,19 @@ function Settings() {
                                     </div>
                                 </div>
                             </div>
-                            <p>RDM Version: {version}</p>
-                            <div className="row text-center">
-                                <div className="col">
-                                    <button className="btn btn-primary" type="button"><i className="bi bi-save-fill"></i> Save</button>
+                            <div className="card-body text-start shadow" style={{ borderRadius: '12px', borderTopLeftRadius: '-1px', opacity: '1', borderColor: 'rgb(0,128,255)', marginBottom: '18px' }}>
+                                <div className="row">
+                                    <div className="col-xl-7">
+                                        <h4><i className="bi bi-info-circle"></i> App Info</h4>
+                                        <h6 className="text-muted mb-2" style={{ marginBottom: '-11px', marginTop: '-4px' }}>Versions and Information</h6>
+                                    </div>
+                                    <div className="col-xl-5" style={{ textAlign: 'right', marginTop: '10px' }}>
+                                        <p>RDM Version: {version}</p>
+                                        <p>{developerInfo}</p>
+                                        <a href="https://github.com/Japerz12138/re-download-manager" target="_blank" rel="noopener noreferrer">
+                                            <img src="https://img.shields.io/github/stars/Japerz12138/re-download-manager?style=social" alt="Github Badge" />
+                                        </a>
+                                    </div>
                                 </div>
                             </div>
                         </div>
